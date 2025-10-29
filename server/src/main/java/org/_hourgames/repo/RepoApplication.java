6252fbecac144dcced6f36ed739de778ed1dc638
Sam Weimer <samweimer7@gmail.com>
2025-10-29 17:32:15 -0400

    refactor: yet another server/backend change

---
+++ server/src/main/java/org/_hourgames/repo/RepoApplication.java
@@ -0,0 +1,13 @@
+package org._hourgames.repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/org/_hourgames/repo/RepoApplicationTests.java
@@ -0,0 +1,13 @@
+package org._hourgames.repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
